975f4c9c2c214f73588591646bcbefdff8a7f4c6
raj07102002 <rajchandrawanshi69@gmail.com>
2025-06-28 17:05:27 +0530

    two

--- 01.pl
+++ 01.pl
@@ -3,4 +3,28 @@
 
 # tips = sns.load_dataset("tips")
 # sns.jointplot(x="total_bill", y="tip", data=tips, kind="scatter")
+# plt.show() 
+
+# Q.2 
+
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import pandas as pd
+
+# # Example data: scores of students
+# data = {
+#     'scores': [40, 50, 60, 60, 70, 70, 70, 80, 80, 90, 100, 100, 100, 100]
+# }
+# df = pd.DataFrame(data)
+
+# # Create the histogram using Seaborn
+# a = sns.displot(df['scores'], kde=False)
+
+# # Set labels and title
+# a.set(xlabel="Student Scores", ylabel="Number of Students", title="Score Distribution")
+
+# # Remove top and right borders
+# sns.despine()
+
+# plt.tight_layout()
 # plt.show()
