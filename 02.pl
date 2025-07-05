f14586177a7a988afe90e9cd867f13e2f440daca
raj07102002 <rajchandrawanshi69@gmail.com>
2025-07-05 23:11:38 +0530

    one

---
+++ 02.pl
@@ -0,0 +1,11 @@
+# Q.1 
+
+# import pandas as pd
+
+# df = pd.DataFrame({
+#     "Sleep Score": ["S98.5", "S87.0", "S91.2"]
+# })
+
+# df["Sleep Score"] = df["Sleep Score"].astype(str).str.slice(1).astype(float)
+
+# print(df)
